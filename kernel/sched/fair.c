b89000407cb62eb83a0641e89e5543466a0baa3a
Lukas0610 <mail@lukasberger.at>
2017-11-16 21:38:51 +0100

    sched: fair: optimize with (un)likely
    Change-Id: Ifd57e2a25bc5f3e07bb91ebf876ac8b9b96a995d

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4993,7 +4993,7 @@ static struct sched_entity *hmp_get_heaviest_task(struct sched_entity* se, int m
 	unsigned long int max_ratio = se->avg.load_avg_ratio;
 	const struct cpumask *hmp_target_mask = NULL;
 
-	if (migrate_up) {
+	if (likely(migrate_up)) {
 		struct hmp_domain *hmp;
 		if(hmp_cpu_is_fastest(cpu_of(se->cfs_rq->rq)))
 			return max_se;
@@ -5005,8 +5005,8 @@ static struct sched_entity *hmp_get_heaviest_task(struct sched_entity* se, int m
 	/* The currently running task is not on the runqueue */
 	se = __pick_first_entity(cfs_rq_of(se));
 
-	while(num_tasks && se) {
-		if (entity_is_task(se)) {
+	while(unlikely(num_tasks && se)) {
+		if (likely(entity_is_task(se))) {
 			if(se->avg.load_avg_ratio > max_ratio &&
 					(hmp_target_mask &&
 					 cpumask_intersects(hmp_target_mask,
@@ -5028,7 +5028,7 @@ static struct sched_entity *hmp_get_lightest_task(struct sched_entity* se, int m
 	unsigned long int min_ratio = ULONG_MAX;
 	const struct cpumask *hmp_target_mask = NULL;
 
-	if (migrate_down) {
+	if (likely(migrate_down)) {
 		struct hmp_domain *hmp;
 		if(hmp_cpu_is_slowest(cpu_of(se->cfs_rq->rq)))
 			return min_se;
@@ -5040,8 +5040,8 @@ static struct sched_entity *hmp_get_lightest_task(struct sched_entity* se, int m
 	/* The currently running task is not on the runqueue */
 	se = __pick_first_entity(cfs_rq_of(se));
 
-	while(num_tasks && se) {
-		if (entity_is_task(se)) {
+	while(unlikely(num_tasks && se)) {
+		if (likely(entity_is_task(se))) {
 			if(se->avg.load_avg_ratio < min_ratio &&
 					(hmp_target_mask &&
 					 cpumask_intersects(hmp_target_mask,
