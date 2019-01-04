2e66515d2e01539685079aaf5c3f43f5625763c9
francitato <franci.ariano@gmail.com>
2019-01-04 12:25:46 +0100

    zip: added aroma

---
+++ build/zip/common/script/flash_kernel.sh
@@ -0,0 +1,17 @@
+#!/sbin/sh
+#
+# ArianoxxKernel Flash script 1.0
+#
+# Thanks to dwander for original script
+# 
+
+bootloader=`getprop ro.bootloader`
+variant=${bootloader:0:4}
+
+cd /tmp/script
+
+tar -Jxf kernel.tar.xz $variant-boot.img
+
+dd of=/dev/block/platform/155a0000.ufs/by-name/BOOT if=/tmp/script/$variant-boot.img
+
+
