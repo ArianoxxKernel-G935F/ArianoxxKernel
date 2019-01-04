2d111a0f0ecece715872916f02518018a06349da
francitato <franci.ariano@gmail.com>
2019-01-04 11:13:58 +0100

    wireless: dhd: Enable standby ARP and multicast packet filters

--- drivers/net/wireless/bcmdhd4359/dhd_linux.c
+++ drivers/net/wireless/bcmdhd4359/dhd_linux.c
@@ -11329,7 +11329,7 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 
 #ifdef PKT_FILTER_SUPPORT
 	/* Setup default defintions for pktfilter , enable in suspend */
-	dhd->pktfilter_count = 6;
+	dhd->pktfilter_count = 5;
 	dhd->pktfilter[DHD_BROADCAST_FILTER_NUM] = NULL;
 	if (!FW_SUPPORTED(dhd, pf6)) {
 		dhd->pktfilter[DHD_MULTICAST4_FILTER_NUM] = NULL;
@@ -11352,8 +11352,10 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 	/* Setup filter to allow only unicast */
 	dhd->pktfilter[DHD_UNICAST_FILTER_NUM] = "100 0 0 0 0x01 0x00";
 #endif /* BLOCK_IPV6_PACKET */
-
+	/* apply APP pktfilter */
+	dhd->pktfilter[DHD_ARP_FILTER_NUM] = "105 0 0 12 0xFFFF 0x0806";
 #ifdef PASS_IPV4_SUSPEND
+	dhd->pktfilter_count = 6;
 	dhd->pktfilter[DHD_MDNS_FILTER_NUM] = "104 0 0 0 0xFFFFFF 0x01005E";
 #else
 	/* Add filter to pass multicastDNS packet and NOT filter out as Broadcast */
